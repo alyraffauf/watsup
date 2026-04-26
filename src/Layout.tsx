a3f5181b1489afe2397bde254ccfa12b28693477
Aly Raffauf <aly@aly.codes>
2026-04-26 12:14:32 -0400

    add subtle gradient

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,7 +2,7 @@ import "./index.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="min-h-screen p-8 max-w-6xl mx-auto bg-zinc-900">
+    <main className="min-h-screen p-8 max-w-6xl mx-auto">
       {/*<header className="mb-12">
         <h1 className="text-4xl font-bold">cute.haus</h1>
       </header>*/}
