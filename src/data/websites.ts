aa585fd173c34df5f259778357582ac37c124ee2
Aly Raffauf <aly@aly.codes>
2026-04-26 12:16:18 -0400

    websites: add atbbs

--- src/data/websites.ts
+++ src/data/websites.ts
@@ -2,5 +2,6 @@ import type { Service } from "../types";
 
 export const websites: Service[] = [
   { name: "aly.codes", url: "https://aly.codes/" },
+  { name: "atbbs", url: "https://atbbs.xyz/" },
   { name: "switchyard.aly.codes", url: "https://switchyard.aly.codes/" },
 ];
