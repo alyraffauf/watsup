1c86ee89fd4e8cbc181f549cd075d80b8622d3e1
Aly Raffauf <aly@aly.codes>
2026-04-26 12:45:26 -0400

    websites: add morgan website

--- src/data/websites.ts
+++ src/data/websites.ts
@@ -4,4 +4,5 @@ export const websites: Service[] = [
   { name: "Aly Raffauf", url: "https://aly.codes/" },
   { name: "atbbs", url: "https://atbbs.xyz/" },
   { name: "Switchyard", url: "https://switchyard.aly.codes/" },
+  { name: "Morgan Tamayo", url: "https://morgantamayo.com/" },
 ];
